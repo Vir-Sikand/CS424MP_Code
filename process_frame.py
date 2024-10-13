46da789f5ffbc8347c025eea68a93206d3afbaf1
Kervi <107320939+pinocchioph1129@users.noreply.github.com>
2024-10-13 16:07:30 -0500

    Add files via upload

---
+++ process_frame.py
@@ -0,0 +1,46 @@
+# UIUC CS 424 Machine Problems 
+# Designed and developed by Md Iftekharul Islam Sakib, Ph.D. Student, CS, UIUC (miisakib@gmail.com) under the supervision of Prof. Tarek Abdelzaher, CS, UIUC (zaher@illinois.edu)
+
+from IoTObjectDetection.IoTObjectDetectionModuleHelperFunctions import *
+from IoTObjectDetection.TaskEntity import *
+
+
+# read the input bounding box data from file
+box_info = read_json_file('../../dataset/waymo_ground_truth_flat.json')
+
+
+def process_frame(frame):
+    """Process frame for scheduling.
+
+    Process a image frame to obtain cluster boxes and corresponding scheduling parameters
+    for scheduling. 
+
+    Student's code here.
+
+    Args:
+        param1: The image frame to be processed. 
+
+    Returns:
+        A list of tasks with each task containing image_path and other necessary information. 
+    """
+
+    cluster_boxes_data = get_bbox_info(frame, box_info)
+    tasks = []
+
+    for bbox in cluster_boxes_data:
+        x1, y1, x2, y2, depth= bbox[:5]
+
+        prio = (x2 - x1) * (y2 - y1)
+
+        task = TaskEntity(image_path= frame.path,
+                          coord=[x1, y1, x2, y2],
+                          priority=prio,
+                          depth=depth
+        )
+        tasks.append(task)
+    
+    return tasks
+    # student's code here
+
+
+    
